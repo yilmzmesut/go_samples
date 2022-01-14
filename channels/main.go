2590ae5829e86d473d68a3db951c4dd208390808
MESUT YILMAZ <yilmaz.mesut@turkcell.com.tr>
2022-01-14 14:54:45 +0300

    go interface and channel examples

---
+++ channels/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, l := range links {
+		go checkLink(l, c)
+	}
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	if _, err := http.Get(link); err != nil {
+		fmt.Println(link, " might be down!")
+		c <- link
+		return
+	}
+	fmt.Println(link, " is up!")
+	c <- link
+}
